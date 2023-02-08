7cd21ad08ae981ea945e01f6c840d55ed8eaba66
loctvl842 <loclepnvx@gmail.com>
2023-02-09 00:15:38 +0700

    update

--- lua/tvl/config/colorizer.lua
+++ lua/tvl/config/colorizer.lua
@@ -7,7 +7,7 @@ colorizer.setup({
   user_default_options = {
     RGB = true, -- #RGB hex codes
     RRGGBB = true, -- #RRGGBB hex codes
-    names = false, -- "Name" codes like Blue or blue
+    name = false, -- "Name" codes like Blue or blue
     RRGGBBAA = true, -- #RRGGBBAA hex codes
     AARRGGBB = true, -- 0xAARRGGBB hex codes
     rgb_fn = true, -- CSS rgb() and rgba() functions
