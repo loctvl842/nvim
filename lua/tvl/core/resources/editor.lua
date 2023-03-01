1d2198dd3abc584429d27995850015f3a4f2ae81
loctvl842 <loclepnvx@gmail.com>
2023-03-01 23:01:35 +0700

    fix: temporarily fix `ft_ignore` of `statuscol`

--- lua/tvl/core/resources/editor.lua
+++ lua/tvl/core/resources/editor.lua
@@ -85,16 +85,23 @@ return {
     config = function()
       local builtin = require("statuscol.builtin")
       require("statuscol").setup({
-        relculright = true,
+        relculright = false,
         segments = {
           { -- line number
-            text = { " ", builtin.lnumfunc },
+            text = { builtin.lnumfunc },
             condition = { true, builtin.not_empty },
             click = "v:lua.ScLa",
           },
           { text = { "%s" },      click = "v:lua.ScSa" }, -- Sign
           { text = { "%C", " " }, click = "v:lua.ScFa" }, -- Fold
         }
+      })
+      vim.api.nvim_create_autocmd({ "BufEnter" }, {
+        callback = function()
+          if vim.bo.filetype == "neo-tree" then
+            vim.o.statuscolumn = ""
+          end
+        end
       })
     end
   },
