30db0ce073b7068b237cb8a10b709d15ebb3bff1
loctvl842 <loclepnvx@gmail.com>
2023-03-22 14:22:16 +0700

    chore: no need mapping here

--- lua/tvl/config/lsp/gitsigns.lua
+++ lua/tvl/config/lsp/gitsigns.lua
@@ -28,26 +28,6 @@ M.on_attach = function(_, buffer)
     end)
     return "<Ignore>"
   end, { expr = true })
-
-  -- Actions
-  map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
-  map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
-  map("n", "<leader>hS", gs.stage_buffer)
-  map("n", "<leader>hu", gs.undo_stage_hunk)
-  map("n", "<leader>hR", gs.reset_buffer)
-  map("n", "<leader>hp", gs.preview_hunk)
-  map("n", "<leader>hb", function()
-    gs.blame_line({ full = true })
-  end)
-  map("n", "<leader>tb", gs.toggle_current_line_blame)
-  map("n", "<leader>hd", gs.diffthis)
-  map("n", "<leader>hD", function()
-    gs.diffthis("~")
-  end)
-  map("n", "<leader>td", gs.toggle_deleted)
-
-  -- Text object
-  map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
 end
 
 return M
