763542e2929065333257ea677fd354c5b8d39e5a
loctvl842 <loclepnvx@gmail.com>
2023-05-30 14:54:34 +0700

    fix: icon ufo

--- lua/tvl/core/resources/editor.lua
+++ lua/tvl/core/resources/editor.lua
@@ -359,7 +359,7 @@ return {
     opts = {
       fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
         local newVirtText = {}
-        local suffix = ("  ...  %d "):format(endLnum - lnum)
+        local suffix = ("  … %d "):format(endLnum - lnum)
         local sufWidth = vim.fn.strdisplaywidth(suffix)
         local targetWidth = width - sufWidth
         local curWidth = 0
