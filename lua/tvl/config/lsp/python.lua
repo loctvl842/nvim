8cd78692e89a562e833397c2286d6571ce4fc007
loctvl842 <loclepnvx@gmail.com>
2023-08-19 10:20:19 +0700

    refactor(python): config pyright
    - try using definition/reference in pyright instead of pylsp
    - mapping for `PyrightOrganizeImports`

--- lua/tvl/config/lsp/python.lua
+++ lua/tvl/config/lsp/python.lua
@@ -2,20 +2,23 @@ local M = {}
 
 M.attach = function(client, buffer)
   local sc = client.server_capabilities
+  local opts = { noremap = true, silent = true }
+  local map = vim.api.nvim_buf_set_keymap
   if client.name == "pylsp" then
     sc.documentFormattingProvider = false
     sc.documentRangeFormattingProvider = false
   end
   if client.name == "pyright" then
-    client.server_capabilities.renameProvider = false -- rope is ok
-    client.server_capabilities.hoverProvider = false -- pylsp includes also docstrings
-    client.server_capabilities.signatureHelpProvider = false -- pyright typing of signature is weird
-    client.server_capabilities.definitionProvider = false -- pyright does not follow imports correctly
-    client.server_capabilities.referencesProvider = false -- pylsp does it
-    client.server_capabilities.completionProvider = {
+    sc.renameProvider = false -- rope is ok
+    sc.hoverProvider = false -- pylsp includes also docstrings
+    sc.signatureHelpProvider = false -- pyright typing of signature is weird
+    sc.definitionProvider = true -- pyright does not follow imports correctly
+    sc.referencesProvider = true -- pylsp does it
+    sc.completionProvider = {
       resolveProvider = true,
       triggerCharacters = { "." },
     }
+    map(buffer, "n", "<leader>lo", "<cmd>PyrightOrganizeImports<CR>", opts)
   end
 end
 
