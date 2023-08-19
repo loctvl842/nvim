4146cbb1509e8f8ebff43b453afddce6c308bcd4
loctvl842 <loclepnvx@gmail.com>
2023-08-19 10:23:36 +0700

    refactor(neo-tree): update config of neo-tree

--- lua/tvl/config/neo-tree/init.lua
+++ lua/tvl/config/neo-tree/init.lua
@@ -74,7 +74,9 @@ local config = {
       hide_dotfiles = false,
       hide_gitignored = false,
     },
-    follow_current_file = true, -- This will find and focus the file in the active buffer every
+    follow_current_file = {
+      enabled = true,
+    }, -- This will find and focus the file in the active buffer every
     -- time the current file is changed while the tree is open.
     group_empty_dirs = true, -- when true, empty folders will be grouped together
   },
