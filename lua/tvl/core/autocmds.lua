205c5a3cfc4befd31284dc039b5926e12ee99af1
loctvl842 <loclepnvx@gmail.com>
2023-10-21 22:03:29 +0700

    chore: update

--- lua/tvl/core/autocmds.lua
+++ lua/tvl/core/autocmds.lua
@@ -42,7 +42,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.wrap = true
-    vim.opt_local.spell = true
+    vim.opt_local.spell = false
   end,
 })
 
