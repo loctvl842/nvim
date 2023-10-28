6d94f5757f7fa27b98a546094377f20ba37a266f
Jordan Faust <jfaust47@gmail.com>
2023-10-28 14:41:35 -0500

    fix: last-jump-position issue with lua_ls + new settings

--- lua/config/lsp/servers.lua
+++ lua/config/lsp/servers.lua
@@ -4,44 +4,60 @@ local servers = {
   html = {},
   jsonls = {},
   lua_ls = {
+    -- enabled = false,
+    -- cmd = { "/home/folke/projects/lua-language-server/bin/lua-language-server" },
+    single_file_support = true,
     settings = {
       Lua = {
-        hint = {
-          enable = true,
-          arrayIndex = "Disable", -- "Enable", "Auto", "Disable"
-          await = true,
-          paramName = "Disable",  -- "All", "Literal", "Disable"
-          paramType = false,
-          semicolon = "Disable",  -- "All", "SameLine", "Disable"
-          setType = true,
-        },
-        runtime = {
-          version = "LuaJIT",
-          special = {
-            reload = "require",
-          },
-        },
-        diagnostics = {
-          globals = { "vim" },
-        },
         workspace = {
-          library = vim.api.nvim_get_runtime_file("", true),
-          -- library = {
-          --   [vim.fn.expand("$VIMRUNTIME/lua")] = false,
-          --   [vim.fn.stdpath("config") .. "/lua"] = false,
-          -- },
           checkThirdParty = false,
         },
         completion = {
-          callSnippet = "Replace",
+          workspaceWord = true,
+          callSnippet = "Both",
         },
         misc = {
           parameters = {
-            "--log-level=trace",
+            -- "--log-level=trace",
           },
         },
-        telemetry = {
-          enable = false,
+        hover = { expandAlias = false },
+        hint = {
+          enable = true,
+          setType = false,
+          paramType = true,
+          paramName = "Disable",
+          semicolon = "Disable",
+          arrayIndex = "Disable",
+        },
+        doc = {
+          privateName = { "^_" },
+        },
+        type = {
+          castNumberToInteger = true,
+        },
+        diagnostics = {
+          disable = { "incomplete-signature-doc", "trailing-space" },
+          -- enable = false,
+          groupSeverity = {
+            strong = "Warning",
+            strict = "Warning",
+          },
+          groupFileStatus = {
+            ["ambiguity"] = "Opened",
+            ["await"] = "Opened",
+            ["codestyle"] = "None",
+            ["duplicate"] = "Opened",
+            ["global"] = "Opened",
+            ["luadoc"] = "Opened",
+            ["redefined"] = "Opened",
+            ["strict"] = "Opened",
+            ["strong"] = "Opened",
+            ["type-check"] = "Opened",
+            ["unbalanced"] = "Opened",
+            ["unused"] = "Opened",
+          },
+          unusedLocalExclude = { "_*" },
         },
         format = {
           enable = true,
