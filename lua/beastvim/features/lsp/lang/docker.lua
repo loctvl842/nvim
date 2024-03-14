457ca881fe540ba6845cb54de68bd82af8515829
loctvl842 <loclepnvx@gmail.com>
2024-03-14 19:41:50 +0700

    feat(lsp): Add support for Dockerfile language server

---
+++ lua/beastvim/features/lsp/lang/docker.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "hadolint" })
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "dockerfile" })
+      end
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        dockerls = {},
+        docker_compose_language_service = {},
+      },
+    },
+  },
+
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.diagnostics.hadolint,
+      })
+    end,
+  },
+}
