f8de6ed30c5cb7a1d64d246235565b5f9a1dbb5d
Jordan Faust <jordan.faust@procore.com>
2024-06-27 20:05:40 -0500

    fix: hide fold column on specific filetypes

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -265,3 +265,14 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     require("session_manager").save_current_session()
   end,
 })
+
+------------------------------- Folds -------------------------------
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "neo-tree", "NeogitStatus" },
+  callback = function()
+    require("ufo").detach()
+    vim.opt_local.foldenable = false
+    vim.opt_local.foldcolumn = "0"
+  end,
+})
