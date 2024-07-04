c7a5ac07d6d3829252d47e97a89b3583eb5f230f
Jordan Faust <jordan.faust@procore.com>
2024-07-04 14:02:59 -0500

    feat(config): remove unused config files + perf fixes

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -152,21 +152,21 @@ return {
   -- Flash enhances the built-in search functionality by showing labels
   -- at the end of each match, letting you quickly jump to a specific
   -- location.
-  -- {
-  --   "folke/flash.nvim",
-  --   event = "VeryLazy",
-  --   vscode = true,
-  --   ---@type Flash.Config
-  --   opts = {},
-  --   -- stylua: ignore
-  --   keys = {
-  --     { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-  --     { "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-  --     { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-  --     { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-  --     { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-  --   },
-  -- },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    vscode = true,
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
+  },
 
   -- which-key helps you remember key bindings by showing a popup
   -- with the active keybindings of the command you started typing.
@@ -208,7 +208,6 @@ return {
         ["<leader>u"] = { name = "+ui" },
         ["<leader>w"] = { name = "+windows" },
         ["<leader>x"] = { name = "+diagnostics/quickfix" },
-        ["<leader>0"] = { "<cmd>Dashboard<cr>", name = "Dashboard" },
       },
       normal = {
         ["<leader>0"] = { "<cmd>Dashboard<CR>", "Dashboard" },
--- lua/plugins/extras/editor/markdown.lua
+++ lua/plugins/extras/editor/markdown.lua
@@ -1,6 +1,7 @@
 return {
   {
     "iamcco/markdown-preview.nvim",
+    event = "VeryLazy",
     filetype = "markdown",
     build = "cd app && yarn install",
   },
