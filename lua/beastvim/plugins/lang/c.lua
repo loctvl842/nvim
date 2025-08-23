ec1e2b7ee2bf6d5f7608889a767649fbf607fe65
AxelRubini <149332378+AxelRubini@users.noreply.github.com>
2025-08-23 22:10:50 +0200

    feat(lang): add cppcheck for C/C++

---
+++ lua/beastvim/plugins/lang/c.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "c", "cpp" } },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed =
+        vim.list_extend(opts.ensure_installed or {}, { "clangd", "clang-format", "cpplint", "cppcheck" })
+    end,
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      servers = {
+        clangd = {
+          config = {
+            cmd = { "clangd" },
+            filetypes = { "c", "cpp", "objc", "objcpp" },
+            root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "compile_flags.txt", ".git" },
+          },
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        c = { "cpplint", "cppcheck" },
+        cpp = { "cpplint", "cppcheck" },
+      },
+    },
+  },
+}
